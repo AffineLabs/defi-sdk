1ebafdb7f8fdc09a8252e433f205f58557e6aba9
Adib Hasan <nzxcvb5@gmail.com>
2021-11-15 20:56:37 -0500

    added function definitions for mv0

--- alpine-defi-sdk/test.js
+++ alpine-defi-sdk/test.js
@@ -2,127 +2,23 @@ import { ethers } from "ethers";
 import { AlpineDeFiSDK } from "./AlpineDeFiSDK.js";
 
 const main = async () => {
-  // run
-  // await window.ethereum.enable()
-  // biconomy integration
-  // the api key is not secret and meant to be used in the frontend
-  // const biconomy = new Biconomy(
-  //     new ethers.providers.Web3Provider(window.ethereum),
-  //     {
-  //         apiKey: "CTGkNtqm8.bce042dc-0ed2-4845-869a-4d178f2465b0",
-  //         debug: true,
-  //     }
-  // );
+  const seed = process.env.MV0_SEED;
   const provider = new ethers.providers.StaticJsonRpcProvider(
     "https://kovan.infura.io/v3/6a4677f9b8014a239fb68742f752fb62"
   );
-  const alpSDK = new AlpineDeFiSDK(provider);
-  const balance = await alpSDK.getUserCurrentBalance(
-    "0x69b3ce79B05E57Fc31156fEa323Bd96E6304852D",
-    "0x6076f3011c987A19a04e2B6a37A96Aed1ee01492",
-    [
-      {
-        inputs: [
-          {
-            internalType: "address",
-            name: "alpUsdcAddress_",
-            type: "address",
-          },
-        ],
-        stateMutability: "nonpayable",
-        type: "constructor",
-      },
-      {
-        inputs: [],
-        name: "alpUsdcAddress",
-        outputs: [
-          {
-            internalType: "address",
-            name: "",
-            type: "address",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-      {
-        inputs: [
-          {
-            internalType: "address",
-            name: "user",
-            type: "address",
-          },
-        ],
-        name: "balanceOf",
-        outputs: [
-          {
-            internalType: "uint256",
-            name: "usdc",
-            type: "uint256",
-          },
-          {
-            internalType: "uint256",
-            name: "alpine",
-            type: "uint256",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-      {
-        inputs: [
-          {
-            internalType: "address",
-            name: "user",
-            type: "address",
-          },
-          {
-            internalType: "uint256",
-            name: "amountUsdc",
-            type: "uint256",
-          },
-        ],
-        name: "deposit",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-      {
-        inputs: [],
-        name: "usdcAddress",
-        outputs: [
-          {
-            internalType: "address",
-            name: "",
-            type: "address",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-      {
-        inputs: [
-          {
-            internalType: "address",
-            name: "user",
-            type: "address",
-          },
-          {
-            internalType: "uint256",
-            name: "amountAlpUsdc",
-            type: "uint256",
-          },
-        ],
-        name: "withdraw",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-    ]
-  );
+
+  const vaultAbi = [{ "inputs": [{ "internalType": "address", "name": "alpUsdcAddress_", "type": "address" }], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "alpUsdcAddress", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "usdc", "type": "uint256" }, { "internalType": "uint256", "name": "alpine", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }, { "internalType": "uint256", "name": "amountUsdc", "type": "uint256" }], "name": "deposit", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "usdcAddress", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }, { "internalType": "uint256", "name": "amountAlpUsdc", "type": "uint256" }], "name": "withdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
+  const vaultAddr = "0x0355b41edb7a29169dfee31c60597835867ba4a7"
+  // const vaultAddr = "0x6076f3011c987A19a04e2B6a37A96Aed1ee01492"
+  const alpSDK = new AlpineDeFiSDK(provider, seed);
+  let balance = await alpSDK.getUserVaultBalance(vaultAddr, vaultAbi);
   console.log({ balance });
-  // log
-  // catch errors
+  // balance = await alpSDK.approveTransaction(vaultAddr, "5");
+  // let response = await alpSDK.buyToken(vaultAddr, vaultAbi, "5");
+  // console.log({ response });
+  // response = await alpSDK.sellToken(vaultAddr, vaultAbi, "2");
+  // console.log({ response });
+
 };
 
 main()
