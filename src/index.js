1853595704e0c1aebd9f9699bc201cc9ff0610ab
larryob <9146641+larryob@users.noreply.github.com>
2022-01-14 18:49:14 -0500

    Use typescript

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import Account from "./Account.js";
-// import MyOtherClass from './my-other-class.js'
-
-// export { MyClass, MyOtherClass }
-export { Account };
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { Account } from "./Account.js";
+import { AlpineDeFiSDK } from "./AlpineDeFiSDK";
+
+export { Account };
