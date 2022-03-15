8081d21a310e45675887f62be9ca82279fced46c
larryob <tosin@tryalpine.com>
2022-03-15 02:05:40 -0400

    Add gas mode to Account class

--- src/frontend/test.ts
+++ src/frontend/test.ts
@@ -3,13 +3,19 @@ import { Account } from "./Account";
 const main = async () => {
   const email = "tosin@multiplyr.ai";
   const alpAccount = new Account();
-  await alpAccount.connect(email, "magic");
+  await alpAccount.connect(email, "metamask");
 
   console.log("before get address");
   const addr = await alpAccount.getUserAddress();
   console.log({ getUserAddress: addr });
 
   let response;
+
+  await alpAccount.setGasMode(true);
+  await alpAccount.buyProduct("alpSave", 10);
+
+  await alpAccount.setGasMode(false);
+  await alpAccount.sellProduct("alpSave", 10);
 
   // console.log("BEFORE APPROVE");
   // response = await alpAccount.approve("alpLarge", "1000000000000000000");
