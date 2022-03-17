f2e831402d585de5f9181d768c3c11213dd42d13
larryob <tosin@tryalpine.com>
2022-03-17 14:54:48 -0400

    Add `getMaticBalance`

--- src/core/test/misc.test.ts
+++ src/core/test/misc.test.ts
@@ -7,7 +7,7 @@ chai.use(solidity);
 const { expect } = chai;
 
 import { AlpineDeFiSDK } from "..";
-import { CONTRACTS, init } from "../cache";
+import { CONTRACTS, init, SIGNER } from "../cache";
 
 const testProvider = new ethers.providers.JsonRpcProvider(
   "http://localhost:8545"
@@ -16,19 +16,31 @@ const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC || "").connect(
   testProvider
 );
 
-before(async () => {
-  await init(testProvider, wallet, undefined);
-});
-it("getUserBalance", async () => {
-  const contracts = CONTRACTS;
-  const beforeBal: ethers.BigNumber = await contracts.usdc.balanceOf(
-    wallet.address
-  );
-  const oneUSDC = ethers.BigNumber.from(10).pow(6);
-  await AlpineDeFiSDK.mintUSDC(wallet.address, 1);
+describe("Balances", async () => {
+  before(async () => {
+    await init(testProvider, wallet, undefined);
+  });
 
-  const afterBal = (await AlpineDeFiSDK.getUserBalance("usdc")).balanceUSDC;
-  console.log({ afterBal });
+  it("getUserBalance", async () => {
+    const { usdc } = CONTRACTS;
+    const beforeBal: ethers.BigNumber = await usdc.balanceOf(wallet.address);
 
-  expect(ethers.utils.parseUnits(afterBal, 6)).to.equal(beforeBal.add(oneUSDC));
+    const oneUSDC = ethers.BigNumber.from(10).pow(6);
+    await AlpineDeFiSDK.mintUSDC(wallet.address, 1);
+
+    const { balanceUSDC: afterBal } = await AlpineDeFiSDK.getUserBalance(
+      "usdc"
+    );
+    console.log({ afterBal });
+
+    expect(ethers.utils.parseUnits(afterBal, 6)).to.equal(
+      beforeBal.add(oneUSDC)
+    );
+  });
+
+  it("getMaticBalance", async () => {
+    const balance = await SIGNER.getBalance();
+    const balStr = await AlpineDeFiSDK.getMaticBalance();
+    expect(ethers.utils.formatEther(balance)).to.equal(balStr);
+  });
 });
