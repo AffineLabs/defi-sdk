719d6f32c07dd1c69981d5f08917cd6c0c4ce58a
larryob <tosin@tryalpine.com>
2022-05-12 23:09:00 -0400

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { Account, ReadAccount } from "./frontend";
-import { AlpineDeFiSDK, AlpineContracts } from "./core";
-
-export { Account, ReadAccount, AlpineDeFiSDK, AlpineContracts };
+import { AlpineDeFiSDK } from "./core";
+import * as types from "./core/types";
+export { Account, ReadAccount, AlpineDeFiSDK, types };
