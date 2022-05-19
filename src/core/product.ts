187125b1213840c05d2b9bf91b62150e0e7a8ab8
Shahjalal Shohag <shahjalalshohag@Shahjalals-MacBook-Air.local>
2022-05-19 20:33:14 +0600

    computed token info using smart contract functions

--- src/core/product.ts
+++ src/core/product.ts
@@ -53,7 +53,7 @@ export async function buyUsdcShares(amountUSDC: number) {
       `Insufficient allowance. Allowance: ${_removeDecimals(
         allowance
       )} USDC, Required: ${amountUSDC} USDC. ` +
-        "Call approve() to increase the allowance."
+      "Call approve() to increase the allowance."
     );
   }
   return blockchainCall(alpSave, "deposit", [amount], {
@@ -108,42 +108,21 @@ export async function getTokenInfo(
   product: AlpineProduct | "usdc"
 ): Promise<TokenInfo> {
   const user = userAddress;
-  if (product === "alpSave") {
-    const { alpSave } = CONTRACTS;
-    const amount: ethers.BigNumber = await alpSave.balanceOf(user);
-    const equity: ethers.BigNumber = await alpSave.tokensFromShares(amount);
-    const price: ethers.BigNumber = await alpSave.tokensFromShares(1e6);
+  if (product === "alpSave" || product === "alpLarge") {
+    const contract: ethers.Contract = CONTRACTS[product];
+    const amount: ethers.BigNumber = await contract.balanceOf(user);
+    const price: ethers.BigNumber = (await contract.detailedPrice()).num;
+    const equity: ethers.BigNumber = amount.mul(price)
+    // token amount has 6 decimals 
+    // and price has 6 decimals for alpSave and 8 decimals for alpLarge
+    const amount_decimals = 6
+    const price_decimals = (product === "alpSave") ? 6 : 8
     return {
-      amount: ethers.utils.formatUnits(amount, 6),
-      price: ethers.utils.formatUnits(price, 6),
-      equity: ethers.utils.formatUnits(equity, 6),
+      amount: ethers.utils.formatUnits(amount, amount_decimals),
+      price: ethers.utils.formatUnits(price, price_decimals),
+      equity: ethers.utils.formatUnits(equity, amount_decimals + price_decimals),
     };
   }
-
-  if (product === "alpLarge") {
-    // alpLarge
-    const { alpLarge } = CONTRACTS;
-    // TODO: let the contract take care of pricing
-    const amount: ethers.BigNumber = await alpLarge.balanceOf(user);
-    const totalDollars: ethers.BigNumber = await alpLarge.valueOfVault();
-    console.log({ totalDollars });
-    // totalDollars * percentOfTotalSupply
-    // dollars given by btc/eth vault actually have 8 decimals
-    const totalSupply: ethers.BigNumber = await alpLarge.totalSupply();
-    const equity = totalDollars.mul(amount).div(totalSupply);
-    console.log({
-      totalDollars: totalDollars.toString(),
-      totalSupply: totalSupply.toString(),
-    });
-    const price = totalDollars.toNumber() / totalSupply.div(1e10).toNumber();
-    console.log({ equity, amount, price });
-    return {
-      amount: ethers.utils.formatUnits(amount, 18),
-      price: price.toString(),
-      equity: ethers.utils.formatUnits(equity, 8),
-    };
-  }
-
   // usdc
   const { usdc } = CONTRACTS;
   const amount = await usdc.balanceOf(user);
--- src/frontend/test.ts
+++ src/frontend/test.ts
@@ -2,6 +2,8 @@ import { Account, ReadAccount } from "./Account";
 
 const main = async () => {
   const email: string = process.env.EMAIL || "";
+  const wallet_address: string = process.env.WALLET_ADDRESS || "";
+
   const alpAccount = new Account();
   console.time("entire-connect");
 
@@ -10,17 +12,19 @@ const main = async () => {
   console.timeEnd("entire-connect");
 
   await alpAccount.setGasMode(true);
-  // await alpAccount.approve("alpSave", "1000000");
+  await alpAccount.approve("alpSave", "1000000");
   await alpAccount.setSimulationMode(true);
-  const receipt = await alpAccount.sellProduct("alpLarge", 1);
+  const receipt = await alpAccount.buyProduct("alpSave", 1);
   console.log({ receipt });
 
-  const readAcc = new ReadAccount("0x69b3ce79B05E57Fc31156fEa323Bd96E6304852D");
+  const readAcc = new ReadAccount(wallet_address);
   await readAcc.init();
   const gas = await readAcc.getGasPrice();
   const balance = await readAcc.getMaticBalance();
-  const info = await readAcc.getTokenInfo("usdc");
-  console.log({ gas, balance, info });
+  const infoAlpSave = await readAcc.getTokenInfo("alpSave");
+  const infoAlpLarge = await readAcc.getTokenInfo("alpLarge");
+  const infoUsdc = await readAcc.getTokenInfo("usdc");
+  console.log({ gas, balance, infoAlpSave, infoAlpLarge, infoUsdc });
 
   console.log("exiting");
 };
