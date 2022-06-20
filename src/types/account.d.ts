c664e63a9e9691a114ad1906de946bee8488e1a6
Prosenjit Chowdhury <prosenjit@tryalpine.com>
2022-06-20 16:31:05 +0600

    updated test file

--- src/types/account.d.ts
+++ src/types/account.d.ts
@@ -1,7 +1,7 @@
 export interface IConnectAccount {
   email: string;
   walletType: "magic" | "metamask";
-  network: "mainnet" | "mumbai";
+  network?: "mainnet" | "mumbai";
   shouldRunMagicTestMode?: boolean;
   getMessage?: (address: string) => Promise<string>;
   verify?: (message: string) => Promise<boolean | undefined>;
