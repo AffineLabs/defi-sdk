61514e4b941357286eed89bade09f46230fe24fe
Prosenjit Chowdhury <94828689+prosenjitalpine@users.noreply.github.com>
2022-07-01 10:42:33 +0600

    Resolved switch Metamask network (#86)

--- src/types/account.ts
+++ src/types/account.ts
@@ -6,3 +6,9 @@ export interface IConnectAccount {
   getMessage?: (address: string) => Promise<string>;
   verify?: (message: string, address: string) => Promise<boolean | undefined>;
 }
+
+export interface MetamaskError {
+  code: number;
+  message: string;
+  stack: string;
+}
