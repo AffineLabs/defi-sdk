0801f74ba193aaa488cd4c28f1039848dd4ec899
Mahmoud Khalifa <mahmoudk@mit.edu>
2022-07-05 13:49:17 -0400

    Added working portfolio testing and functions

--- src/core/portfolio.ts
+++ src/core/portfolio.ts
@@ -14,24 +14,20 @@ export async function portfolioUpdate(buyAmounts: productAmounts, sellAmounts: p
     const sellAmount = sellAmounts[product];
     if (sellAmount === undefined || Number(sellAmount) === 0)
     {
-      console.log("Skipped: ", product)
       continue;
     }
     const usdcAmount = _addDecimals(sellAmount.toString());
     let iface = router.interface;
     data.push((iface as any).encodeFunctionData("withdraw", [CONTRACTS[product].address ,userAddress, usdcAmount, ethers.BigNumber.from(2).pow(256).sub(1)]));
   }
-  console.log("Passed Withdraw")
   for (const product of alpineProducts) {
       const buyAmount = buyAmounts[product];
       if (buyAmount === undefined || Number(buyAmount) === 0)
         continue;
       const usdcAmount = _addDecimals(buyAmount.toString());
-      console.log(`${product}: ${usdcAmount}`);
       let iface = router.interface;
       data.push((iface as any).encodeFunctionData("depositToVault", [CONTRACTS[product].address ,userAddress, usdcAmount, 0])); 
   }
-  console.log("Data: ", data);
   return blockchainCall(router, "multicall", [data]);
 }
 
@@ -40,7 +36,6 @@ export async function portfolioPurchase(allocations: productAllocation, amount:n
   for (const product of alpineProducts) {
       buyAmounts[product] = (allocations[product]!*amount/ALLOCSUM).toString();
   }
-  console.log({buyAmounts});
   return portfolioUpdate(buyAmounts, {});
 }
 
@@ -49,7 +44,6 @@ export async function portfolioSell(allocations:productAllocation, amount:number
   for (const product of alpineProducts) {
       sellAmounts[product] = (allocations[product]!*amount/ALLOCSUM).toString();
   }
-  console.log({sellAmounts});
   return portfolioUpdate({}, sellAmounts);
 }
 
@@ -59,14 +53,14 @@ export async function portfolioRebalance(allocations:productAllocation){
   const user = userAddress;
   const contracts = CONTRACTS;
   const coinBalance:productBalances = {};
-  const total: ethers.BigNumber = ethers.BigNumber.from(0);
+  let total: ethers.BigNumber = ethers.BigNumber.from(0);
   const buyAmounts:productAmounts = {};
   const sellAmounts:productAmounts = {};
   for (const product of alpineProducts){
       const contract = CONTRACTS[product];
       const tokenBalance: ethers.BigNumber = await contract.balanceOf(user);
       const dollarsBalance = await tokensFromShares(product, tokenBalance);
-      total.add(dollarsBalance!);
+      total = total.add(dollarsBalance!);
       coinBalance[product] = dollarsBalance;
   }
   for (const product of alpineProducts){
@@ -75,11 +69,11 @@ export async function portfolioRebalance(allocations:productAllocation){
         const currentBalance = coinBalance[product]!;
         if (idealAmount > currentBalance){
           const amount = idealAmount.sub(currentBalance);
-          buyAmounts[product] = _removeDecimals(amount.div(await CONTRACTS[product].decimals())).toString();
+          buyAmounts[product] = _removeDecimals(amount);
         }
         else if(currentBalance > idealAmount){
           const amount = currentBalance.sub(idealAmount);
-          sellAmounts[product] = _removeDecimals(amount.div(await CONTRACTS[product].decimals())).toString();
+          sellAmounts[product] = _removeDecimals(amount);
         }
   }
   return portfolioUpdate(buyAmounts, sellAmounts);
--- src/core/test/portfolio.test.ts
+++ src/core/test/portfolio.test.ts
@@ -16,7 +16,6 @@ describe("Portfolio transactions", async () => {
     setProvider(testProvider);
     await init(wallet, undefined);
     await PROVIDER.send("evm_setAccountBalance", [userAddress, ethers.BigNumber.from(10).pow(18).toHexString()]);
-    console.log("Balance: ", await wallet.getBalance());
     await mintUSDC(wallet.address, 10000);
     await approve("router", "1000000");
     await blockchainCall(CONTRACTS.router, "approve", [CONTRACTS.usdc.address ,CONTRACTS.alpLarge.address, ethers.BigNumber.from(2).pow(256).sub(1)]);
@@ -36,7 +35,6 @@ describe("Portfolio transactions", async () => {
     for (const product of alpineProducts){
       const contract = CONTRACTS[product];
       const tokenBalance: ethers.BigNumber = await contract.balanceOf(user);
-      console.log(`Token Balance: ${product} `, tokenBalance);
       const dollarsBalance = await tokensFromShares(product, tokenBalance);
       coinBalance[product] = dollarsBalance;
     }
@@ -46,14 +44,11 @@ describe("Portfolio transactions", async () => {
 
   });
   it("Portfolio Sell", async () => {
-    console.log("Allowance: ",await (await CONTRACTS.usdc.allowance(CONTRACTS.router.address, CONTRACTS.alpSave.address)));
     const coinBalance:productBalances = {};
     let allocation:productAllocation = {};
     allocation['alpSave'] = 100;
     allocation['alpLarge'] = 0;
-    console.log("Balance: ", await getTokenInfo("usdc"));
     await portfolioSell(allocation, 100);
-    console.log("Balance: ", await getTokenInfo("usdc"));
     const user = userAddress;
     const total: ethers.BigNumber = ethers.BigNumber.from(0);
     for (const product of alpineProducts){
@@ -62,30 +57,26 @@ describe("Portfolio transactions", async () => {
       const dollarsBalance = await tokensFromShares(product, tokenBalance);
       total.add(dollarsBalance!);
       coinBalance[product] = dollarsBalance;
-      console.log("Dollars: ", _removeDecimals(dollarsBalance!));
     }
     assert(Number(_removeDecimals(coinBalance['alpSave']!)) > 395);
     assert(Number(_removeDecimals(coinBalance['alpSave']!)) < 400);
   });
-  // it("Portfolio Rebalance", async () => {
-  //   const allocation:productAllocation = {};
-  //   allocation['alpLarge'] = 50;
-  //   allocation['alpSave'] = 50;
-  //   await portfolioPurchase(allocation, 1000);
-  //   allocation['alpLarge'] = 30;
-  //   allocation['alpSave'] = 70;
-  //   await portfolioRebalance(allocation);
-  //   const user = userAddress;
-  //   const coinBalance:productBalances = {};
-  //   const total: ethers.BigNumber = ethers.BigNumber.from(0);
-  //   for (const product of alpineProducts){
-  //     const contract = CONTRACTS[product];
-  //     const tokenBalance: ethers.BigNumber = await contract.balanceOf(user);
-  //     const dollarsBalance = await tokensFromShares(product, tokenBalance);
-  //     total.add(dollarsBalance!);
-  //     coinBalance[product] = dollarsBalance;
-  //   }
-  //   assert(coinBalance['alpLarge']! == ethers.BigNumber.from(300));
-  //   assert(coinBalance['alpSave']! == ethers.BigNumber.from(700));
-  // });
+  it("Portfolio Rebalance", async () => {
+    const allocation:productAllocation = {};
+    allocation['alpLarge'] = 50;
+    allocation['alpSave'] = 50;
+    await portfolioRebalance(allocation);
+    const user = userAddress;
+    const coinBalance:productBalances = {};
+    const total: ethers.BigNumber = ethers.BigNumber.from(0);
+    for (const product of alpineProducts){
+      const contract = CONTRACTS[product];
+      const tokenBalance: ethers.BigNumber = await contract.balanceOf(user);
+      const dollarsBalance = await tokensFromShares(product, tokenBalance);
+      total.add(dollarsBalance!);
+      coinBalance[product] = dollarsBalance;
+    }
+    assert(Number(_removeDecimals(coinBalance['alpLarge']!))+ 5 > Number(_removeDecimals(coinBalance['alpSave']!)));
+    assert(Number(_removeDecimals(coinBalance['alpLarge']!))- 5 < Number(_removeDecimals(coinBalance['alpSave']!)));
+  });
 });
