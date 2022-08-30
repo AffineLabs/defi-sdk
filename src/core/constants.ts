0d9a203295af9707eb5ada0a0c24bfd7a76cfb25
Prosenjit Chowdhury <94828689+prosenjitalpine@users.noreply.github.com>
2022-08-31 02:00:32 +0600

    Updated provider url (#105)
    * updated provider url
    
    * upgraded version
    
    * upgraded package version
    
    * upgraded package version

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -3,3 +3,6 @@ export const DEFAULT_WALLET = "magic";
 // The chain id is a hexadecimal string preceeded by "0x"
 const rawId = process.env.CHAIN_ID || "80001";
 export const CHAIN_ID = `0x${Number(rawId).toString(16)}`;
+
+export const NETWORK_TYPE: "mumbai" | "mainnet" =
+  process.env.NEXT_PUBLIC_NETWORK_TYPE === "mainnet" ? "mainnet" : "mumbai";
