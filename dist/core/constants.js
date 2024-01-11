8d36a52466492eba00121f22835c5f541b35eac1
Prosenjit Chowdhury <94828689+pchy10@users.noreply.github.com>
2024-01-11 18:51:08 +0600

    build latest changes & upgrade version (#161)

--- dist/core/constants.js
+++ dist/core/constants.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.NETWORK_PARAMS = exports.CCIP_NETWORK_SELECTOR = exports.FORKED_NODE_URL_FOR_BASE = exports.FORKED_NODE_URL_FOR_MATIC = exports.FORKED_NODE_URL_FOR_ETH = exports.IS_USING_FORKED_MAINNET = exports.WALLETCONNECT_PROJECT_ID = exports.MAX_APPROVAL_AMOUNT = exports.MAX_UINT = exports.ALLOWED_CHAIN_IDS = exports.NETWORK_TYPE = exports.getChainIdFromRaw = exports.DEFAULT_RAW_CHAIN_ID = exports.DEFAULT_WALLET = void 0;
+exports.NETWORK_PARAMS = exports.WITHDRAW_SLIPPAGE_BY_PRODUCT = exports.CCIP_NETWORK_SELECTOR = exports.FORKED_NODE_URL_FOR_BASE = exports.FORKED_NODE_URL_FOR_MATIC = exports.FORKED_NODE_URL_FOR_ETH = exports.IS_USING_FORKED_MAINNET = exports.WALLETCONNECT_PROJECT_ID = exports.MAX_APPROVAL_AMOUNT = exports.MAX_UINT = exports.ALLOWED_CHAIN_IDS = exports.NETWORK_TYPE = exports.getChainIdFromRaw = exports.DEFAULT_RAW_CHAIN_ID = exports.DEFAULT_WALLET = void 0;
 const ethers_1 = require("ethers");
 exports.DEFAULT_WALLET = "magic";
 // The chain id is a hexadecimal string preceeded by "0x"
@@ -23,6 +23,35 @@ exports.FORKED_NODE_URL_FOR_BASE = process.env.FORKED_NODE_URL_FOR_BASE || "";
 exports.CCIP_NETWORK_SELECTOR = {
     1: `5009297550715157269`,
     137: `4051577828743386545`,
+};
+/**
+ * We will use this to show the slippage for the withdraw/ sell of the product/ baskets.
+ * * Note: Please add the slippage here if you want to include a new product.
+ */
+exports.WITHDRAW_SLIPPAGE_BY_PRODUCT = {
+    ethLeverage: {
+        max: 1,
+        avg: 0.4,
+    },
+    ethEarn: {
+        max: 1,
+        avg: 0.5,
+    },
+    alpLarge: {
+        max: 1,
+        avg: 0.5,
+    },
+    ethWethEarn: {
+        max: 1,
+        avg: 0.5,
+    },
+    alpSave: {},
+    degen: {},
+    polygonDegen: {},
+    polygonLeverage: {},
+    baseUsdEarn: {},
+    baseLeverage: {},
+    ssvEthUSDEarn: {},
 };
 exports.NETWORK_PARAMS = {
     1: {
