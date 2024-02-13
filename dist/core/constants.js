daecc9e991a1472f662f592e123e2ea96f9c34f2
Prosenjit Chowdhury <prosenjit@tryalpine.com>
2024-02-13 14:57:35 +0600

    updated rpc url for matic mumbai

--- dist/core/constants.js
+++ dist/core/constants.js
@@ -88,7 +88,7 @@ export const NETWORK_PARAMS = {
             symbol: "MATIC",
             decimals: 18,
         },
-        rpcUrls: ["https://matic-mumbai.chainstacklabs.com"],
+        rpcUrls: ["https://polygon-mumbai.g.alchemy.com/v2/demo"],
         blockExplorerUrls: ["https://mumbai.polygonscan.com"],
     },
     8453: {
